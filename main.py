a0a7635370bd737df93c35cc8651fec0ac98a0f7
abdelrahman.gaber <abdelrahmangabermohamed@gmail.com>
2026-07-25 15:29:49 +0300

    updated main.py to accept user input

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #This is a test file
+
 def main(): 
     print("hello world!")
     return 0
 
 main()
+while True:
+    choice = int(input("Choose: "))
+    match choice:
+        case 1: print("Welcome"); continue
+        case 2: print("Exit"); break
+        case default: print("Invalid input"); continue
