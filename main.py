14901c74bac0cfefe41e997fb31248016ee72d9f
abdelrahman.gaber <abdelrahmangabermohamed@gmail.com>
2026-07-27 23:07:52 +0300

    Cleaning up repo

--- main.py
+++ main.py
@@ -2,9 +2,3 @@ def main():
     print("hello world!")
     return 0
 main()
-while True:
-    choice = int(input("Choose: "))
-    match choice:
-        case 1: print("Welcome"); continue
-        case 2: print("Exit"); break
-        case default: print("Invalid input"); continue
